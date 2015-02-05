8ad3d47f30a5d6932c043d35eca4f825ec8ad4a3
sanbei120 <jonlinpei@gmail.com>
2015-02-05 11:20:29 -0800

    update

---
+++ fakeDropbox/photosViewController.swift
@@ -0,0 +1,45 @@
+//
+//  photosViewController.swift
+//  fakeDropbox
+//
+//  Created by Jonlin Pei on 2/5/15.
+//  Copyright (c) 2015 Jonlin Pei. All rights reserved.
+//
+
+import UIKit
+
+class photosViewController: UIViewController {
+
+    
+    @IBOutlet weak var photosScrollView: UIScrollView!
+    
+    @IBOutlet weak var photosImage: UIImageView!
+    
+    
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+        
+        photosScrollView.contentSize = photosImage.frame.size
+    
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepareForSegue(segue: UIStoryboardSegue, sender: AnyObject?) {
+        // Get the new view controller using segue.destinationViewController.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
